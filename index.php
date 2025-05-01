15fe17eb878b6abd83fbefbdd173812a2f41636a
Aleksandra Bainczyk <aleksandra_bainczyk@ainslay.local>
2025-05-01 11:55:38 +0200

    Pierwsza wersja apteczki

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Apteczka domowa</title>
+</head>
+<body>
+    <h1>Witaj w apteczce domowej!</h1>
+    <p>Tu będzie lista leków</p>
+    <?php
+    echo "<p>Dzisiaj jest: " . date("Y-m-d H:i:s") . "</p>";
+    ?>
+</body>
+</html>
